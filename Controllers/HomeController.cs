36416b52dbd214197c5499bd3f72c407a53d99ca
Alex Luciano <alex.luciano.topan@gmail.com>
2025-02-24 21:49:06 -0300

    adicionando tratamento de erros no controller de categorias

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlogAPI.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet("")]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
